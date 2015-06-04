d09e4dfe0a3861718074450d5ba5bb0836bd47f5
Simon Urli <simon.urli@gmail.com>
2015-06-04 18:34:35 +0200

    SERVICES-44 Creation of a Source Interface to manage refactoring of sources

---
+++ scripts/server/SourceItf.ts
@@ -0,0 +1,58 @@
+/**
+ * @author Simon Urli <simon@the6thscreen.fr>
+ */
+
+/// <reference path="./SourceNamespaceManager.ts" />
+/// <reference path="../Logger.ts" />
+
+/**
+ * A SourceItf describes a Source to call inside a service.
+ *
+ * Each SourceItf must declare a method 'run' which will be executed using the provided params.
+ */
+class SourceItf {
+
+	/**
+	 * The SourceNamespaceManager associated with a source
+	 *
+	 * @property _sourceNamespaceManager
+	 * @type SourceNamespaceManager
+	 */
+	private _sourceNamespaceManager : SourceNamespaceManager;
+
+	/**
+	 * The params used by the source call
+	 *
+	 * @property _params
+	 * @type any
+	 */
+	private _params : any;
+
+	constructor(params : any, sourceNamespaceManager : SourceNamespaceManager) {
+		this.setParams(params);
+		this.setSourceNamespaceManager(sourceNamespaceManager);
+
+		this.run();
+	}
+
+	public setParams(params : any) {
+		this._params = params;
+	}
+
+	public setSourceNamespaceManager(sourceNamespaceManager : SourceNamespaceManager) {
+		this._sourceNamespaceManager = sourceNamespaceManager;
+	}
+
+	public getParams() : any {
+		return this._params;
+	}
+
+	public getSourceNamespaceManager() : NamespaceManager {
+		return this._sourceNamespaceManager;
+	}
+
+	public run() {
+		Logger.error("The method run() should be implemented.");
+	}
+
+}
