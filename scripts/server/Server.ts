f1d36d1fc56fa8f7f8f3ff45b32ccafb6074f19b
Christian Brel <ch.brel@gmail.com>
2015-10-06 17:29:01 +0200

    refs COREBACK-18 : Added some logs.

--- scripts/server/Server.ts
+++ scripts/server/Server.ts
@@ -308,6 +308,9 @@ class Server {
 	 * @returns null if not found, NamespaceManager if found
 	 */
     retrieveNamespaceManagerFromSocketId(socketId : string) {
+		Logger.debug("Server - retrieveNamespaceManagerFromSocketId");
+		Logger.debug(this.namespaceManagers);
+		Logger.debug(socketId);
 		if(typeof(this.namespaceManagers[socketId]) == "undefined") {
 			return null;
 		} else {
